1610dcce8e7ba75b72da43578699816cbd5e4a02
Rhino <ryanharrisfl@gmail.com>
2019-04-20 13:20:55 -0400

    Port is listening on 3000 - tested and confirmed.

--- server.js
+++ server.js
@@ -1,6 +1,45 @@
-var express = require("express");
-var morgan = require("morgan");
-var mongoose = require("morgan");
 
-var axios = ("axios");
-var cherrios = ("cherrios");
+var express = require("express");
+var mongoose = require("mongoose");
+
+// These are required for 
+//var axios = ("axios");
+//var cherrios = ("cherrios");
+
+// Requiring models from models folder
+//var db = require("./models");
+
+// creating port for routes
+var PORT = 3000;
+
+var app = express();
+
+// Start of Middle Ware config 
+
+//Uses logger to show requests
+//app.use(logger("dev"));
+
+//Parsing body as JSON
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Creates static folder
+app.use(express.static("public"));
+
+
+//Creating connection to MongoDB database
+
+mongoose.connect("mongodb://localhost/newsscraper", { useNewUrlParser: true});
+
+
+
+
+
+
+// Starts up the server
+
+// Start the server
+app.listen(PORT, function() {
+    console.log("App running on port " + PORT + "!");
+  });
+  
